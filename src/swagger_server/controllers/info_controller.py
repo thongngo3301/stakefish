85eb2e43561d19063bd8cd1762c57f23f7647d5e
Thong Ngo <thongngo3301@gmail.com>
2024-05-21 19:40:29 +0700

    update

--- src/swagger_server/controllers/info_controller.py
+++ src/swagger_server/controllers/info_controller.py
@@ -1,8 +1,11 @@
 import connexion
 import six
+import time
+import os
 
 from swagger_server.models.inline_response2001 import InlineResponse2001  # noqa: E501
 from swagger_server import util
+from flask import make_response, jsonify
 
 
 def get_root():  # noqa: E501
@@ -13,4 +16,19 @@ def get_root():  # noqa: E501
 
     :rtype: InlineResponse2001
     """
-    return 'do some magic!'
+    # Get the current UNIX epoch time
+    current_time = int(time.time())
+
+    # Check if the application is running in Kubernetes
+    kubernetes = os.environ.get('KUBERNETES_SERVICE_HOST') is not None
+
+    # Create the response data
+    response_data = InlineResponse2001(
+        version="0.1.0",
+        date=current_time,
+        kubernetes=kubernetes
+    )
+
+    # Create the response with the response code 200
+    response = make_response(jsonify(response_data.dict()), 200)
+    return response
--- src/swagger_server/test/test_info_controller.py
+++ src/swagger_server/test/test_info_controller.py
@@ -20,8 +20,21 @@ class TestInfoController(BaseTestCase):
         response = self.client.open(
             '/',
             method='GET')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+        self.assert200(response, 'Expected response code 200')
+
+        # Parse the response data
+        response_data = json.loads(response.data.decode('utf-8'))
+
+        # Validate the response data structure
+        self.assertIsInstance(response_data, dict)
+        self.assertIn('version', response_data)
+        self.assertIn('date', response_data)
+        self.assertIn('kubernetes', response_data)
+
+        # Validate the type of each response data field
+        self.assertIsInstance(response_data['version'], str)
+        self.assertIsInstance(response_data['date'], int)
+        self.assertIsInstance(response_data['kubernetes'], bool)
 
 
 if __name__ == '__main__':
